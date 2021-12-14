301dfc55fcc8f889e6f8806543232fb952c83258
leliev <lelievrevincent@free.fr>
2021-12-14 14:15:32 +0100

    token set to 6h

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -46,7 +46,7 @@ exports.login = (req, res, next) => {
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: jwt.sign({ userId: user._id }, process.env.TKEY, { expiresIn: '24h' }
+                        token: jwt.sign({ userId: user._id }, process.env.TKEY, { expiresIn: '6h' }
                         )
                     });
                 })
